fbe0421c5e5cf31cbc4da8f050c7787298e63495
zlomke76-del <zlomke76@gmail.com>
2026-01-22 14:32:50 -0600

    Update page.tsx

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -25,7 +25,10 @@ import { exportCanon } from "@/lib/export/exportCanon";
 
 import ResolutionDraftPanel from "@/components/resolution/ResolutionDraftPanel";
 import NextActionHint from "@/components/NextActionHint";
-import WorldLedgerPanel from "@/components/world/WorldLedgerPanel";
+import WorldLedgerPanelLegacy from "@/components/world/WorldLedgerPanel.legacy";
+
+<WorldLedgerPanelLegacy events={state.events} />
+
 import DungeonPressurePanel from "@/components/world/DungeonPressurePanel";
 
 import StewardedShell from "@/components/layout/StewardedShell";
